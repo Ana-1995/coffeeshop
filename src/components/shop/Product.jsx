c35e5c5d68c35c6e822ec10428506150850c31e5
Anano <ananoiashagashvili@gmail.com>
2023-10-17 15:28:05 +0400

    updated

--- src/components/shop/Product.jsx
+++ src/components/shop/Product.jsx
@@ -4,8 +4,10 @@ import { useCart } from '../cart/CartContext';
 const Product = ({ img, name, price, id }) => {
     const { addToCart } = useCart()
     const [isClicked, setIsClicked]=useState(false)
+ const [quantity, setQuantity] = useState(0)
 
     const handleAddToCart = () => {
+      setQuantity(quantity +1)
       addToCart({
         id: id,
         img,
@@ -19,14 +21,21 @@ const Product = ({ img, name, price, id }) => {
       className='bg-transparent min-w-[300px] h-auto p-4 flex flex-col justify-center relative rounded-lg'
       key={id}
     >
-      <button onClick={handleAddToCart} className={`absolute top-2 font-sans right-2 text-yellow-900  px-2 py-1 rounded-xl font-bold duration-300 hover:bg-yellow-400 cursor-pointer ${isClicked ? 'bg-yellow-500' : 'bg-yellow-300'}`}>
-       {isClicked ? 'Ordered' : 'Order'}
+      <button
+        onClick={handleAddToCart}
+        className={`absolute top-2 font-sans right-2 text-yellow-900  px-2 py-1 rounded-xl font-bold duration-300 hover:bg-yellow-400 cursor-pointer ${
+          isClicked ? 'bg-yellow-500' : 'bg-yellow-300'
+        }`}
+      >
+        {isClicked ? `Ordered(${quantity})` : 'Order'}
       </button>
       <div className='flex flex-col text-center'>
         <div className='w-full h-72 overflow-hidden rounded-lg'>
           <img src={img} alt={name} className='w-full h-full object-cover' />
         </div>
-        <p className='pt-2 font-semibold text-amber-100 hover:text-amber-700 duration-300 text-2xl'>{name}</p>
+        <p className='pt-2 font-semibold text-amber-100 hover:text-amber-700 duration-300 text-2xl'>
+          {name}
+        </p>
         <p className='text-center text-amber-400 font-bold text-2xl font-mono'>
           €{price}
         </p>
